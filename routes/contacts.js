d08f03fcfbe7c0de2ad44c10050ae501e9b4f869
TheGeneticsGuy <aaronjtopping@gmail.com>
2025-05-03 18:20:36 -0700

    Added the contacts controller and route

---
+++ routes/contacts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const contactsController = require('../controllers/contactsController');
+
+// GET all contacts
+router.get('/', contactsController.getAllContacts);
+
+// GET single contact by ID
+// Note: ':id' is a route parameter. I found that Express makes it available in req.params.id
+router.get('/:id', contactsController.getSingleContact);
+
+// Maybe add POST, DEL, PUT parameters later here? Future project prob...
+
+module.exports = router;
